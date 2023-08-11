59f64fb50e04736627afc7dee21765c8f986cdc7
heonq <gkagjsrb@naver.com>
2023-08-11 17:54:23 +0900

    feat(Validator): 유효성을 검사한 후 예외 시 alert를 통해 메시지를 출력하고 false를 리턴하도록 수정

--- utils/Validator.js
+++ utils/Validator.js
@@ -3,9 +3,20 @@ import { CONSTANTS } from './Constants.js';
 
 export const Validator = {
   validateInput(numbers) {
-    if (numbers.length !== CONSTANTS.validLength) return ERROR_MESSAGE.invalidLength;
-    if (new Set(numbers).size !== CONSTANTS.validLength) return ERROR_MESSAGE.duplicated;
-    if (!numbers.every((number) => CONSTANTS.minNumber <= number && number <= CONSTANTS.maxNumber))
-      return ERROR_MESSAGE.outOfRange;
+    if (numbers.length !== CONSTANTS.validLength) {
+      alert(ERROR_MESSAGE.invalidLength);
+      return false;
+    }
+    if (new Set(numbers).size !== CONSTANTS.validLength) {
+      alert(ERROR_MESSAGE.duplicated);
+      return false;
+    }
+    if (
+      !numbers.every((number) => CONSTANTS.minNumber <= number && number <= CONSTANTS.maxNumber)
+    ) {
+      alert(ERROR_MESSAGE.outOfRange);
+      return false;
+    }
+    return true;
   },
 };
