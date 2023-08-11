369b280c00563c94c8178ba10890fe8224491b7b
heonq <gkagjsrb@naver.com>
2023-08-11 11:41:46 +0900

    feat: 기능 구현에 필요한 상수 파일 생성

---
+++ utils/Constants.js
@@ -0,0 +1,9 @@
+export const CONSTANTS = Object.freeze({
+  ball: '볼',
+  strike: '스트라이크',
+  nothing: '낫싱',
+  minNumber: 1,
+  maxNumber: 9,
+  validLength: 3,
+  winStrike: 3,
+});
---
+++ utils/ErrorMessage.js
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGE = Object.freeze({
+  invalidLength: '3자리의 숫자를 입력해주세요.',
+  duplicated: '각 자리가 중복되지 않은 수를 입력해주세요.',
+  outOfRange: '각 자리가 1에서 9사이인 3자리 숫자를 입력해주세요.',
+});
