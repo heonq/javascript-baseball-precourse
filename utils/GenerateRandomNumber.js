fcf9a0f413f202209e257f38ab0de6459cf4c436
heonq <gkagjsrb@naver.com>
2023-08-11 12:50:48 +0900

    feat: 랜덤 3자리 숫자 생성하는 기능 구현

---
+++ utils/GenerateRandomNumber.js
@@ -0,0 +1,8 @@
+export default generateRandomNumber = () => {
+  let answer = [];
+  while (answer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!answer.includes(number)) answer.push(number);
+  }
+  return answer.join('');
+};
