ab2abcc59903d136abce5faa7eb5bb385d14c103
heonq <gkagjsrb@naver.com>
2023-08-11 13:12:23 +0900

    feat: 사용자가 입력한 값의 볼,스트라이크 결과 구하는 기능 구현

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import GenerateRandomNumber from '../utils/GenerateRandomNumbers.js';
 
 export default class BaseballGame {
   #computerNumbers;
+  #userInputNumbers;
 
   constructor() {
     this.init();
   }
   init() {
     this.#computerNumbers = GenerateRandomNumbers();
+  }
+
+  countBall(computerInputNumbers, userInputNumbers) {
+    return userInputNumbers.filter(
+      (number, index) =>
+        computerInputNumbers.includes(number) && computerInputNumbers[index] !== number,
+    ).length;
+  }
+  countStrike(computerInputNumbers, userInputNumbers) {
+    return userInputNumbers.filter((number, index) => computerInputNumbers[index] === number)
+      .length;
   }
 
   play(computerInputNumbers, userInputNumbers) {}
